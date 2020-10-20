bfc710bddb18d0a3c7624882b3abcc35a0636400
David Zuber <zuber.david@gmx.de>
2020-10-20 19:58:23 +0100

    New layout and fix reset

--- keyboards/handwired/dactyl_manuform/6x6/keymaps/storax/keymap.c
+++ keyboards/handwired/dactyl_manuform/6x6/keymaps/storax/keymap.c
@@ -12,15 +12,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_QWERTY] = LAYOUT_6x6(
 
-     KC_F1  , KC_F2 , KC_F3 , KC_F4 , KC_F5 , KC_F6 ,                         KC_F7 , KC_F8 , KC_F9 ,KC_F10 ,KC_F11 ,KC_F12 ,
+     KC_F1  , KC_F2 , KC_F3 , KC_F4 , KC_F5 , KC_F6 ,                         KC_F7 , KC_F8 , KC_F9 ,KC_F10 ,KC_F11 ,RESET ,
      KC_ESC , KC_1  , KC_2  , KC_3  , KC_4  , KC_5  ,                         KC_6  , KC_7  , KC_8  , KC_9  , KC_0  ,KC_BSLASH,
      KC_LCTL, KC_Q  , KC_W  , KC_E  , KC_R  , KC_T  ,                         KC_Y  , KC_U  , KC_I  , KC_O  , KC_P  ,KC_MINS,
      KC_LSFT, KC_A  , KC_S  , KC_D  , KC_F  , KC_G  ,                         KC_H  , KC_J  , KC_K  , KC_L  ,KC_SCLN,KC_QUOT,
-     KC_LCTL, KC_Z  , KC_X  , KC_C  , KC_V  , KC_B  ,                         KC_N  , KC_M  ,KC_COMM,KC_DOT ,KC_SLSH,KC_RSFT,
+     KC_TAB , KC_Z  , KC_X  , KC_C  , KC_V  , KC_B  ,                         KC_N  , KC_M  ,KC_COMM,KC_DOT ,KC_SLSH,KC_RSFT,
                       KC_LBRC,KC_RBRC,                                                       KC_PLUS, KC_EQL,
-                                      RAISE,KC_ENT,                        KC_SPC, LOWER,
-                                      KC_TAB,KC_HOME,                         KC_END,  KC_LALT,
-                                      KC_BSPC, KC_GRV,                        KC_LGUI, KC_DEL
+                                      KC_BSPC, KC_ENT,                        KC_SPC, LOWER,
+                                      KC_LALT,KC_HOME,                        KC_END,  KC_RALT,
+                                        RAISE, KC_GRV,                        KC_LGUI, KC_DEL
   ),
 
   [_LOWER] = LAYOUT_6x6(
